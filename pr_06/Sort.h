c30bff0f0b5f9c95e0e02e3c6d53d7d5a8863d69
neapollll <102368404+neapollll@users.noreply.github.com>
2023-12-03 20:06:04 +0300

    Add files via upload

---
+++ pr_06/Sort.h
@@ -0,0 +1,57 @@
+﻿#include<vector>
+#include <algorithm>
+#include <iostream>
+//#include <cstddef>
+
+template<typename T>
+void bubbleSort(std::vector<T>& vec)
+{
+    T tmp;
+    for (int i = 0; i < vec.size() - 1; i++) {
+        //if(i % 1000 == 0) std::cout << i <<std::endl;
+        for (int j = 0; j < vec.size() - 1 - i; j++) {
+            if (vec[j] > vec[j + 1]) {
+                tmp = vec[j + 1];
+                vec[j + 1] = vec[j];
+                vec[j] = tmp;
+            }
+        }
+    }
+}
+
+template<typename T>
+void fastSort(std::vector<T>& vec)
+{
+    std::sort(vec.begin(), vec.end());
+}
+
+template<typename T>
+int partition(std::vector<T>& arr, int low, int high) {
+    T pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+template<typename T>
+void quickSort(std::vector<T>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+template<typename T>
+void quickSort(std::vector<T>& arr) {
+    quickSort(arr, 0, arr.size() - 1);
+}
---
+++ pr_06/main.cpp
@@ -0,0 +1,225 @@
+﻿#include <iostream>
+#include <vector>
+#include "Sort.h"
+#include <chrono>
+#include <cstdlib> 
+#include <iterator>
+//#include <cstddef>
+
+using namespace std;
+
+template<typename T>
+unsigned long long durationFastSort(T& base_vec) // ms
+{
+    auto copy = base_vec;
+    auto start = chrono::high_resolution_clock::now();
+    fastSort(copy);
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+    return duration.count();
+}
+
+template<typename T>
+unsigned long long durationQuickSort(T& base_vec) // ms
+{
+    auto copy = base_vec;
+    auto start = chrono::high_resolution_clock::now();
+    quickSort(copy);
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+    return duration.count();
+}
+
+template<typename T>
+unsigned long long durationBubbleSort(T& base_vec) // ms
+{
+    auto copy = base_vec;
+    auto start = chrono::high_resolution_clock::now();
+    bubbleSort(copy);
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+    return duration.count();
+}
+
+void display_vector(const vector<double>& v)
+{
+    copy(v.begin(), v.end(),
+        ostream_iterator<double>(cout, " "));
+}
+
+int main()
+{
+
+    vector<int> int_vec = {};
+    for (int i = 0; i < 1000000; i++) {
+        int_vec.push_back(rand());
+    }
+    vector<double> double_vec = {};
+    for (int i = 0; i < 1000000; i++) {
+        double_vec.push_back(rand()); 
+    }
+
+    cout << "\n\tvec<int>[1'000'000] standart fast sort test :\n\n";
+    cout << "\ti\t|\tduration\n";
+    int min = -1;
+    int max = -1;
+    double average = 0.;
+    int duration;
+    for (int i = 0; i < 10; i++) {
+        duration = durationFastSort(int_vec);
+        cout << "\t" << i + 1 << "\t|\t" << duration << "ms\n";
+        if (min < 0) min = duration;
+        if (max < 0) max = duration;
+        if (max < duration) max = duration;
+        if (min > duration) min = duration;
+        average += duration;
+    }
+    average /= 10.;
+
+    cout << "\n";
+    cout << "\t" << "min : " << min << endl;
+    cout << "\t" << "max : " << max << endl;
+    cout << "\t" << "average : " << average << endl;
+
+    cout << "______________________________________________________" << endl;
+
+
+    cout << "\n\tvec<double>[1'000'000] standart fast sort test :\n\n";
+    cout << "\ti\t|\tduration\n";
+    min = -1;
+    max = -1;
+    average = 0.;
+
+    for (int i = 0; i < 10; i++) {
+        duration = durationFastSort(double_vec);
+        cout << "\t" << i + 1 << "\t|\t" << duration << "ms\n";
+        if (min < 0) min = duration;
+        if (max < 0) max = duration;
+        if (max < duration) max = duration;
+        if (min > duration) min = duration;
+        average += duration;
+    }
+    average /= 10.;
+
+    cout << "\n";
+    cout << "\t" << "min : " << min << endl;
+    cout << "\t" << "max : " << max << endl;
+    cout << "\t" << "average : " << average << endl;
+
+
+    cout << "______________________________________________________" << endl;
+    // пузырьковая сортировка для вектора размером 1000000 слишком долгая, возьмем вектор размером 10000
+    vector<int> int_vec_bub = {};
+    for (int i = 0; i < 10000; i++) {
+        int_vec_bub.push_back(rand());
+    }
+    vector<double> double_vec_bub = {};
+    for (int i = 0; i < 10000; i++) {
+        double_vec_bub.push_back(rand()); 
+    }
+
+    cout << "\n\tvec<int>[10'000] bubble sort test :\n\n";
+    cout << "\ti\t|\tduration\n";
+    min = -1;
+    max = -1;
+    average = 0.;
+
+    for (int i = 0; i < 10; i++) {
+        duration = durationBubbleSort(int_vec_bub);
+        cout << "\t" << i + 1 << "\t|\t" << duration << "ms\n";
+        if (min < 0) min = duration;
+        if (max < 0) max = duration;
+        if (max < duration) max = duration;
+        if (min > duration) min = duration;
+        average += duration;
+    }
+    average /= 10.;
+
+    cout << "\n";
+    cout << "\t" << "min : " << min << endl;
+    cout << "\t" << "max : " << max << endl;
+    cout << "\t" << "average : " << average << endl;
+
+    cout << "______________________________________________________" << endl;
+
+    cout << "\n\tvec<double>[10'000] bubble sort test :\n\n";
+    cout << "\ti\t|\tduration\n";
+    min = -1;
+    max = -1;
+    average = 0.;
+
+    for (int i = 0; i < 10; i++) {
+        duration = durationBubbleSort(double_vec_bub);
+        cout << "\t"<<i + 1 << "\t|\t" << duration << "ms\n";
+        if (min < 0) min = duration;
+        if (max < 0) max = duration;
+        if (max < duration) max = duration;
+        if (min > duration) min = duration;
+        average += duration;
+    }
+    average /= 10.;
+
+    cout << "\n";
+    cout << "\t" << "min : " << min << endl;
+    cout << "\t" << "max : " << max << endl;
+    cout << "\t" << "average : " << average << endl;
+
+    cout << "______________________________________________________" << endl;
+    vector<int> int_vec_q = {};
+    for (int i = 0; i < 1000000; i++) {
+        int_vec_q.push_back(rand());
+    }
+    vector<double> double_vec_q = {};
+    for (int i = 0; i < 1000000; i++) {
+        double_vec_q.push_back(rand());
+    }
+
+    cout << "\n\tvec<int>[1'000'000] fast sort test :\n\n";
+    cout << "\ti\t|\tduration\n";
+    min = -1;
+    max = -1;
+    average = 0.;
+
+    for (int i = 0; i < 10; i++) {
+        duration = durationQuickSort(int_vec_q);
+        cout << "\t" << i + 1 << "\t|\t" << duration << "ms\n";
+        if (min < 0) min = duration;
+        if (max < 0) max = duration;
+        if (max < duration) max = duration;
+        if (min > duration) min = duration;
+        average += duration;
+    }
+    average /= 10.;
+
+    cout << "\n";
+    cout << "\t" << "min : " << min << endl;
+    cout << "\t" << "max : " << max << endl;
+    cout << "\t" << "average : " << average << endl;
+
+    cout << "______________________________________________________" << endl;
+
+
+    cout << "\n\tvec<double>[1'000'000] fast sort test :\n\n";
+    cout << "\ti\t|\tduration\n";
+    min = -1;
+    max = -1;
+    average = 0.;
+
+    for (int i = 0; i < 10; i++) {
+        duration = durationQuickSort(double_vec_q);
+        cout << "\t" << i + 1 << "\t|\t" << duration << "ms\n";
+        if (min < 0) min = duration;
+        if (max < 0) max = duration;
+        if (max < duration) max = duration;
+        if (min > duration) min = duration;
+        average += duration;
+    }
+    average /= 10.;
+
+    cout << "\n";
+    cout << "\t" << "min : " << min << endl;
+    cout << "\t" << "max : " << max << endl;
+    cout << "\t" << "average : " << average << endl;
+
+    return 0;
+}
